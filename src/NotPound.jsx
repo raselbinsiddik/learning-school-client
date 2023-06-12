3a224186b56cb4ddd6cdf2ef1f3bb82a9ebfc9e9
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-06-12 13:47:49 +0600

    add added

--- src/NotPound.jsx
+++ src/NotPound.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import not from '../src/assets/404.gif'
 
 const NotPound = () => {
     return (
-        <div>
-            <img src={not} alt="" />
+        <div className='text-center'>
+            <img className='w-96' src={not} alt="" />
             <div className='text-3xl text-center mt-8'>do not find your information</div>
+            <Link to="/"><button className='btn btn-warning'>Home</button></Link>
         </div>
     );
 };
